4bf96e622d1b582b79236f9ed8265fb3ca0f0f50
VectorSophie <jay7math@gmail.com>
2024-12-28 21:16:36 +0900

    yay

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import { auth } from "@/lib/auth";
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import localFont from 'next/font/local';
+import { auth } from '@/lib/auth';
+import './globals.css';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
 
 export const metadata: Metadata = {
-  title: "Bookmark",
-  description: "idk lol",
+  title: 'BookMark',
+  description: 'Book & Mark',
 };
 
 export default async function RootLayout({
@@ -13,13 +25,28 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = auth()
+  const session = await auth();
+  const didLogin = !!session?.user;
+  console.log('🚀  session:', session);
+
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
-        className="font-sans antialiased"
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header>
+          <Link href={didLogin ? '/api/auth/signout' : '/api/auth/signin'}>
+            {didLogin ? session.user?.name : 'SignIn'}
+          </Link>
+        </header>
+        <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+          <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
+            {children}
+          </main>
+          <footer className='fixed bottom-0'>
+            &#169; indiflex SeniorCoding
+          </footer>
+        </div>
       </body>
     </html>
   );
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import GithubProvider from 'next-auth/providers/github';
+import NaverProvider from 'next-auth/providers/naver';
+import KakaoProvider from 'next-auth/providers/kakao'
 export const {
   handlers: { GET, POST },
   auth, signIn, signOut,
@@ -14,6 +16,14 @@ export const {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID as string,
+      clientSecret: process.env.NAVER_CLIENT_SECRET as string,
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAKO_CLIENT_ID as string,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+    })
   ],
   secret: process.env.AUTH_SECRET as string,
 })
